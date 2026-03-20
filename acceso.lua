5023b2acd74e4e63d0302d780048d83532fd904a
lynox-67 <lynoxhub67@gmail.com>
2026-03-19 19:56:37 -0600

    Update acceso.lua

--- acceso.lua
+++ acceso.lua
@@ -1,10 +1,6 @@
 return {
-    FREE_WEEK = true,
+    FREE_DAY = false,
+    FREE_DAY = true,
     ALLOWED_USERS = {
         "N4rczis0",
-        --"bicampeonmx0", -- 10000 hora empieza 1:20 termina 10:24
-        "24kboi_mo", -- publicidad
-        "Jhon1432",
         "bicampeonmx0",
-    }
-}
