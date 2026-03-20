066af3b78a9b45210a13638e2d28e120c453abc7
lynox-67 <lynoxhub67@gmail.com>
2026-03-19 19:46:19 -0600

    Create acceso.lua

---
+++ acceso.lua
@@ -0,0 +1,10 @@
+return {
+    FREE_DAY = false,
+    ALLOWED_USERS = {
+        "N4rczis0",
+        --"XxsebasxX9009", -- 10000 hora empieza 1:20 termina 3:24
+        "24kboi_mo", -- publicidad
+        "Jhon1432",
+        "bicampeonmx0",
+    }
+}
